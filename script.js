4b40b493bd51bb73765ac23a902e7b3b2e7c4170
guileiteh <151942780+guileiteh@users.noreply.github.com>
2024-07-25 10:39:52 -0300

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    document.querySelector('.burger').classList.toggle('active');
+});
