ba6e6bea326af22d2a6c680223ef02a10d2235b8
guileiteh <151942780+guileiteh@users.noreply.github.com>
2024-08-02 08:36:59 -0300

    .

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     menuButton.addEventListener('click', function () {
         const isOpen = navMenu.style.transform === 'translateX(0%)';
-        navMenu.style.transform = isOpen ? 'translateX(-100%)' : 'translateX(0%)';
+        navMenu.style.transform = isOpen ? 'translateX(100%)' : 'translateX(0%)';
         menuButton.classList.toggle('open', !isOpen);
     });
 });
