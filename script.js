82b8a8e60e4ad615c5ad2ae7fa73d0a2ba80220d
guileiteh <151942780+guileiteh@users.noreply.github.com>
2024-08-02 08:30:56 -0300

    .

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
         menuButton.classList.toggle('open', !isOpen);
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('menuButton');
+    const navMenu = document.getElementById('navMenu');
+
+    menuButton.addEventListener('click', function () {
+        const isOpen = navMenu.style.transform === 'translateX(0%)';
+        navMenu.style.transform = isOpen ? 'translateX(-100%)' : 'translateX(0%)';
+        menuButton.classList.toggle('open', !isOpen);
+    });
+});
